efd45ccb2afc0720eab4e433544a31afb830c1e2
patman <27874300+patmanf@users.noreply.github.com>
2023-04-14 13:33:00 -0500

    added cheese 2 to cooking collection & update dependencies

--- items/generic/shop/cheese2.lua
+++ items/generic/shop/cheese2.lua
@@ -5,13 +5,17 @@ local cheese2 = {name = "cheese", parameters = {
 	shortdescription = "Cheese 2"
 }}
 
+function init()
+	knowsCheese = player.blueprintKnown(cheese2)
+	script.setUpdateDelta(knowsCheese and 0 or 10)
+end
+
 function update(dt)
-	if not player.blueprintKnown(cheese2) then
+	if not knowsCheese then
 		if player.blueprintKnown{name = "cheese"} then
 			player.giveBlueprint(cheese2)
 			script.setUpdateDelta(0)
+			knowsCheese = true
 		end
-	else
-		script.setUpdateDelta(0)
 	end
 end
