8d534e057b860e21417cce96329880bc56e473e8
patman <27874300+patmanf@users.noreply.github.com>
2024-09-08 20:45:51 -0500

    the localous

--- pat/cheese2/builder.lua
+++ pat/cheese2/builder.lua
@@ -1,6 +1,6 @@
 function build(_, _, params)
 	local cheese = root.itemConfig({name = "cheese", parameters = params})
-	config, parameters = cheese.config, cheese.parameters
+	local config, parameters = cheese.config, cheese.parameters
 
 	local cheese2 = root.assetJson("/pat/cheese2/cheese2.recipe:output").parameters
 	parameters = sb.jsonMerge(parameters, cheese2)
